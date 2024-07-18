bf9bd066456bfb576a85256a8d2baea69185580d
Cristian García <cristiangp2001@gmail.com>
2024-07-18 14:40:12 -0600

    Carga de interfaces reservations.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import SignIn from "./pages/SignIn";
 import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp.jsx";
+import Reservations from "./pages/Reservations.jsx";
 
 export default function App() { // Modificado para seguir las convenciones de ES6
     return (
@@ -16,6 +17,7 @@ export default function App() { // Modificado para seguir las convenciones de ES
                 <Route path="/appointments" element={<Appointments/>}/>
                 <Route path="/signin" element={<SignIn/>}/>
                 <Route path="/signup" element={<SignUp/>}/>
+                <Route path="/reservations" element={<Reservations/>}/>
 
             </Routes>
             <Footer/>
